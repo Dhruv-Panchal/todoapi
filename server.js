9807f9c45d9d6d176f85c54693c354c5f4e285a6
Dhruv Panchal <dhruvpanchal96@gmail.com>
2017-09-02 09:01:04 +0000

    recent commit

--- server.js
+++ server.js
@@ -9,13 +9,33 @@ var todos = [{
     id: 2,
     description: 'Go to market',
     completed: false
+}, {
+    id: 3,
+    description: "feed the cat",
+    completed: true
 }];
 
 app.get('/', function(req, res) {
-    res.send("todo api root");
+    res.send("ToDo Api root");
 });
 app.get('/todos', function(req, res) {
-    response.json(todos);
+    res.json(todos);
+});
+app.get('/todos/:id', function(req, res) {
+    var todoId = parseInt(req.params.id, 10);
+
+    var matchedTodo;
+    todos.forEach(function(todo) {
+        if (todoId === todo.id) {
+            matchedTodo = todo;
+        }
+    });
+    if (matchedTodo) {
+        res.json(matchedTodo);
+    }
+    else {
+        res.status(404).send();
+    }
 });
 app.listen(PORT, function() {
     console.log('Express running on port ' + PORT);
