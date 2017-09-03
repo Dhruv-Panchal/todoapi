25a7dcdce6167d2e3c7068bf97a03b8736ce8504
Dhruv Panchal <dhruvpanchal96@gmail.com>
2017-09-03 16:10:13 +0000

    for lappy

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var bodyBarser = require('body-parser');
+var _ = require('underscore');
 var PORT = process.env.PORT || 3000;
 var todos = [];
 var todoNextId = 1;
@@ -30,6 +31,9 @@ app.get('/todos/:id', function(req, res) {
 });
 
 app.post('/todos', function(req, res) {
+    var body = req.body;
+
+    console.log(descriptipon)
 
 });
 app.listen(PORT, function() {
