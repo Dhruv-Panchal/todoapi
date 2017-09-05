23eac2c2621f0d2ade5cb197f9bd6fec27f1b2ee
Dhruv Panchal <dhruvpanchal96@gmail.com>
2017-09-05 04:57:09 +0000

    fixed

--- playground/database.js
+++ playground/database.js
@@ -18,21 +18,46 @@ var Todo = sequelize.define('todo', {
         defaultValue: false
     }
 });
-sequelize.sync({
-    force: true
 
+sequelize.sync({
+    // force: true
 }).then(function() {
     console.log('Everything is synced');
 
-    Todo.create({
-        description: 'walk the dog',
-        completed: false
-    }).then(function(todo) {
-        return Todo.create({
-            description: 'Clean office'
-        });
-    }).then(function() {}).catch(function(e) {
-        console.log(e);
+    Todo.findById(3).then(function(todo) {
+        if (todo) {
+            console.log(todo.toJSON());
+        }
+        else {
+            console.log('Todo not found');
+        }
     });
 
+    // Todo.create({
+    // 	description: 'Take out trash'
+    // }).then(function(todo) {
+    // 	return Todo.create({
+    // 		description: 'Clean office'
+    // 	});
+    // }).then(function() {
+    // 	// return Todo.findById(1)
+    // 	return Todo.findAll({
+    // 		where: {
+    // 			description: {
+    // 				$like: '%Office%'
+    // 			}
+    // 		}
+    // 	});
+    // }).then(function (todos) {
+    // 	if (todos) {
+    // 		todos.forEach(function (todo) {
+    // 			console.log(todo.toJSON());
+    // 		});
+
+    // 	} else {
+    // 		console.log('no todo found!');
+    // 	}
+    // }).catch(function(e) {
+    // 	console.log(e);
+    // });
 });
